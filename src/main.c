e0f316c1bbfa4a1f5f485aa2f0d1d0d4c9fef40a
netoe1 <elynetobr@outlook.com>
2023-11-24 11:02:48 -0300

    adding pushWithBranches

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <locale.h>
 #include <string.h>
 
+// Internal
+#define BUF_MAX 512
+
 #define USAGE "Usage : <program> <operation>"
 #define EXIT -1
 #define SUCCESS 0
@@ -10,6 +13,10 @@
 #define UPDATE_SHORT "-up"
 #define HELP_LONG "--help"
 #define HELP_SHORT "-h"
+#define UPDATE_BRANCH_LONG "--update-branch"
+#define UPDATE_BRANCH_SHORT "-ub"
+#define SCRIPT_UPDATE_STR "git pull\ngit add --all\ngit commit -m \"updated with git-for-beginners\"\ngit push origin main"
+#define SCRIPT_UPDATE_BRANCH_STR "git pull\n git add --all\n git commit -m \"updated with git-for-beginners\"\ngit push origin %s"
 
 #define GIT_UPDATE() \
     system("git pull\n git add --all\n git commit -m \"updated with git-for-beginners\"\ngit push origin main")
@@ -22,28 +29,55 @@
 #define INVALID_USAGE() \
     puts("ERROR: Type <program> --help or -h, to see valid commands!")
 
+#define label argv[2]
+#define operation argv[1]
+
+void gitUpdate();    // Implemented as macro
+void helpCli();      // Implemented as macro
+void invalidUsage(); // Implemented as macro
+
+void gitUpdateBranch();
+
 int main(int argc, char **argv)
 {
     if (argc != 1)
     {
-        if (argv[1] == NULL)
+        if (operation == NULL)
         {
             return EXIT;
         }
         if (
-            (strcmp(argv[1], UPDATE_SHORT) == 0 || strcmp(argv[1], UPDATE_LONG) == 0))
+            (strcmp(operation, UPDATE_LONG) == SUCCESS || strcmp(operation, UPDATE_SHORT) == SUCCESS))
         {
             GIT_UPDATE();
             return SUCCESS;
         }
         else if (
-            (strcmp(argv[1], HELP_LONG) == 0 || strcmp(argv[1], HELP_SHORT) == 0))
+            (strcmp(operation, HELP_LONG) == SUCCESS || strcmp(operation, HELP_SHORT) == SUCCESS))
         {
             HELP_CLI();
             return SUCCESS;
+        }
+        else if ((strcmp(operation, UPDATE_BRANCH_LONG) == SUCCESS || strcmp(operation, UPDATE_BRANCH_SHORT) == SUCCESS) &&
+                 (strcmp(operation, label)) == SUCCESS)
+        {
+            gitUpdateBranch(label); // This function already treat errors.
         }
     }
 
     INVALID_USAGE();
     return EXIT;
+}
+
+void gitUpdateBranch(char *branch)
+{
+    if (branch == NULL)
+    {
+        puts("ERROR: Invalid input for branch.");
+        return;
+    }
+
+    char buffer[BUF_MAX];
+    sprintf(buffer, SCRIPT_UPDATE_STR, branch);
+    system(buffer);
 }
